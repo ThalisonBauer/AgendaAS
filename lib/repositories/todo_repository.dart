6d77bc1de6c4bde71deae71d0e9f0b7d8436af3c
Thalison Bauer <71103256+ThalisonBauer@users.noreply.github.com>
2020-12-06 13:39:36 -0300

    Refactoring

--- lib/repositories/todo_repository.dart
+++ lib/repositories/todo_repository.dart
@@ -7,22 +7,12 @@ class TodoRepository {
   final url = 'https://jsonplaceholder.typicode.com/users';
 
   TodoRepository([Dio contact]) {
-    if (contact == null) {
-      this.dio = Dio();
-    } else {
-      this.dio = contact;
-    }
+    dio = contact ?? Dio();
   }
 
   Future<List<TodoModel>> fetchTodos() async {
     final response = await dio.get(url);
     final list = response.data as List;
-
-    List<TodoModel> todos = [];
-    for (var json in list) {
-      final todo = TodoModel.fromJson(json);
-      todos.add(todo);
-    }
-    return todos;
+    return list.map((json) => TodoModel.fromJson(json)).toList();
   }
 }
