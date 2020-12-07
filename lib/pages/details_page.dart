1c22597b41796cc44b16520235821b5fbae36598
Thalison Bauer <71103256+ThalisonBauer@users.noreply.github.com>
2020-12-07 20:11:44 -0300

    detailsOK

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -23,18 +23,59 @@ class _DetailsPageState extends State<DetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Contatos'),
+        title: Text('Detalhes do contato'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(24),
         child: Column(
           children: [
+            Center(
+              child: Image.network(
+                'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png',
+                scale: 3,
+              ),
+            ),
             Row(
               children: [
-                Center(
-                  child: Image.network(
-                    'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png',
-                    scale: 3,
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Nome: ' + widget.name + ' ' + widget.username,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Email: ' + widget.email,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Telefone: ' + widget.phone,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
                   ),
                 )
               ],
